90ca11017339df563c0e15f0ab35cd7ba4c5a573
C0deR4t <monte@ekronds.co.za>
2023-04-08 08:44:33 -0600

    feat!: Created first autocompletion and syntax highlighting

---
+++ installPRLDSyntax.bat
@@ -0,0 +1,8 @@
+@echo off
+echo Ensure Notepad++ is installed then continue here with the PRLD Pseudocode syntax install
+pause
+cd /D "%~dp0"
+xcopy pseudoCode_UDL.xml %appdata%\Notepad++\userDefineLangs /y
+xcopy PRLDpseudocode.xml "C:\Program Files\Notepad++\autoCompletion" /y
+Please close and re-open Notepad++
+pause
