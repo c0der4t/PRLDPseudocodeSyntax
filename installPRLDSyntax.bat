b8553199f764a3871dc013235f36060126dca512
c0der4t <monte@ekronds.co.za>
2023-04-19 11:47:29 +0200

    feat: Added highlight support for the for..endFor feat: Added new functions - ToUpperCase and equals

--- installPRLDSyntax.bat
+++ installPRLDSyntax.bat
@@ -4,5 +4,5 @@ pause
 cd /D "%~dp0"
 xcopy pseudoCode_UDL.xml %appdata%\Notepad++\userDefineLangs /y
 xcopy PRLDpseudocode.xml "C:\Program Files\Notepad++\autoCompletion" /y
-Please close and re-open Notepad++
+echo Please close and re-open Notepad++
 pause
